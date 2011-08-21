c86211718bc214baca6b6be08755dce79acdf3cb
Bob Copeland <me@bobcopeland.com>
2011-08-21 10:58:46 -0400

    Add breadth-first-search, Makefile, and metis distribution improvements

--- Makefile
+++ Makefile
@@ -1,12 +1,58 @@
-all: pagerank shortest_paths
+SRCS=pagerank.cc shortest_paths.cc metis_dist_mod.hpp bfs.cc centrality.cc
+DISTFILES=$(SRCS) Makefile README
+DISTDIR=copeland-final-project
+DISTNAME=$(DISTDIR).tar.gz
 
-CXX=mpicxx
-CXXFLAGS=-Wno-deprecated
+# Set this if you want to build with a local install of boost
+# by default assumes /usr/
+# BOOST_ROOT=boost_inst
+# LIBS=-L$(BOOST_ROOT)/lib -lboost_graph_parallel -lboost_graph \
+#	-lboost_system -lboost_mpi -lboost_date_time -lboost_serialization \
+#   -lboost_regex
 
-LIBS=-lboost_graph_parallel -lboost_system
+BOOST_ROOT=/usr
+LIBS=-lboost_graph_parallel -lboost_system -lboost_date_time
+
+# Set CXX to MPECXX and build MPE2 in order to use MPI log facility
+MPE_HOME=mpe2-1.3.0
+MPECXX=$(MPE_HOME)/bin/mpecc -mpicc=$(MPICXX) -mpilog
+
+# Set this define if you want to use boost version of
+# metis_distribution which is _very_ slow
+# DEF=-DUSE_SLOW_METIS
+
+MPICXX=mpicxx
+CXX=$(MPICXX)
+CXXFLAGS=-Wno-deprecated -g $(DEF) -I$(BOOST_ROOT)/include
+
+all: pagerank shortest_paths bfs centrality
+
+clean:
+	$(RM) *.o *.ps *.tar.gz *.log *.aux *.sw? pagerank shortest_paths
+
+%.dvi: %.tex
+	latex $<
+	latex $<
 
 pagerank: pagerank.o
 	$(CXX) $(CXXFLAGS) -o pagerank pagerank.o $(LIBS)
 
+bfs: bfs.o
+	$(CXX) $(CXXFLAGS) -o bfs bfs.o $(LIBS)
+
 shortest_paths: shortest_paths.o
 	$(CXX) $(CXXFLAGS) -o shortest_paths shortest_paths.o $(LIBS)
+
+centrality: centrality.o
+	$(CXX) $(CXXFLAGS) -o centrality centrality.o $(LIBS)
+
+#writeup.pdf: writeup.dvi
+#	dvipdf writeup.dvi
+
+dist: clean
+	mkdir -p build/$(DISTDIR) && \
+	cp -R $(DISTFILES) build/$(DISTDIR) && \
+	cd build && \
+	tar czvf ../$(DISTNAME) $(DISTDIR)
+	$(RM) -r build
+
---
+++ bfs.cc
@@ -0,0 +1,97 @@
+#include <boost/graph/use_mpi.hpp>
+#include <boost/graph/breadth_first_search.hpp>
+#include <boost/graph/distributed/adjacency_list.hpp>
+#include <boost/graph/distributed/mpi_process_group.hpp>
+#include <boost/random/linear_congruential.hpp>
+#include <boost/graph/metis.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+#include <limits.h>
+#include "metis_dist_mod.hpp"
+
+using namespace boost;
+using boost::graph::distributed::mpi_process_group;
+using boost::posix_time::ptime;
+using namespace boost::graph;
+
+typedef adjacency_list<vecS, distributedS<mpi_process_group, vecS>,
+                       directedS,
+                       property<vertex_distance_t, size_t> > graph_t;
+
+typedef graph_traits<graph_t>::vertex_descriptor vertex_t;
+typedef std::pair<int, int> edge_t;
+typedef property_map<vertex_t, float> rank_map_t;
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int my_id;
+    vertex_t *v;
+
+    mpi::environment env(argc, argv);
+
+    std::ifstream in_graph(argv[1]);
+    metis_reader reader(in_graph);
+    mpi_process_group pg;
+
+    int start = atoi(argv[3]);
+
+    ptime now(boost::posix_time::microsec_clock::universal_time());
+    if (process_id(pg) == 0)
+        std::cout << "start: " << now << std::endl;
+
+    std::ifstream in_partitions(argv[2]);
+
+#if USE_SLOW_METIS
+    metis_distribution dist(in_partitions, process_id(pg));
+#else
+    metis_distribution_mod dist(in_partitions, pg);
+#endif
+
+    graph_t g(reader.begin(), reader.end(),
+            reader.num_vertices(), pg, dist);
+
+    my_id = process_id(g.process_group());
+
+    property_map<graph_t, vertex_distance_t>::type distance =
+        get(vertex_distance, g);
+
+    BGL_FORALL_VERTICES(v, g, graph_t) 
+    {
+        put(distance, v, INT_MAX);
+    }
+    vertex_t vstart = vertex(start, g);
+    put(distance, vstart, 0);
+
+    ptime load_time(boost::posix_time::microsec_clock::universal_time());
+    if (my_id == 0)
+        std::cout << "graph loaded in " << (load_time - now) << std::endl;
+
+    ptime start_time(boost::posix_time::microsec_clock::universal_time());
+
+    breadth_first_search(g, vstart,
+        visitor(make_bfs_visitor(record_distances(distance, on_tree_edge()))));
+
+    synchronize(g.process_group());
+
+    ptime end_time(boost::posix_time::microsec_clock::universal_time());
+    if (my_id == 0)
+        std::cout << "ran in " << (end_time-start_time) << " s\n";
+
+#if 0
+    for (i=0 ; i < reader.num_vertices(); i++)
+    {
+        vertex_t v = vertex(i, g);
+        if (owner(v) == my_id)
+        {
+            size_t distance = get(get(vertex_distance, g), v);
+            std::cout << i << ": " << distance << std::endl;
+        }
+    }
+#endif
+
+
+    return 0;
+}
